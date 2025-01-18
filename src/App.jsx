da5b66372edca2e434aa1dc73027611142548978
Justin <jcare@glynlyon.com>
2025-01-17 19:42:26 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <>
+        <h1 className="text-3xl font-bold mb-4">Conway's Game of Life</h1>
+        <div className="container w-3/4 h-4/5 border-4 border-white rounded-md"></div>
+    </>
+  )
+}
+
+export default App
